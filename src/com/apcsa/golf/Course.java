722dff7d6a32b71c5804c8a3c8b166b608f0c53d
Sean <smjaejin@gmail.com>
2017-05-24 12:54:21 -0700

    the hole

---
+++ src/com/apcsa/golf/Course.java
@@ -0,0 +1,18 @@
+package com.apcsa.golf;
+
+/**
+ * Created by apcsaper5 on 5/22/17.
+ */
+
+import java.util.ArrayList;
+public class Course {
+
+
+    //public ArrayList<Obstacle> courseObjects;
+
+    public Course(){
+        //courseObjects = new ArrayList();
+    }
+
+
+}
