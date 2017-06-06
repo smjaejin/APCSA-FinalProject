5aa8e759466e809fb49ac35ff05c44a9dcf40467
harrison <harrison@greco.com>
2017-06-06 15:49:37 -0700

    the hole

--- src/com/apcsa/golf/WaterTrap.java
+++ src/com/apcsa/golf/WaterTrap.java
@@ -1,16 +1,25 @@
 package com.apcsa.golf;
 import edu.princeton.cs.introcs.StdDraw;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
 
 /**
+ * if the ball enters the WaterTrap, it is moved to the location of the last swing, and movement is stopped
  * Created by apcsaper5 on 5/24/17.
  */
 public class WaterTrap extends Wall {
 
-    public WaterTrap(double x1, double y1, double halfWidth, double halfHeight) {
 
+
+    public WaterTrap(double x1, double y1, double halfWidth, double halfHeight) {
         super(x1,y1,halfWidth,halfHeight);
+        color = new Color(0,105,148);
+    }
+
+    void draw() {
+        StdDraw.setPenColor(color);
+        StdDraw.filledRectangle(x1, y1, halfWidth, halfHeight);
     }
 
     void interact(Ball b) {
@@ -18,7 +27,7 @@ public class WaterTrap extends Wall {
         if (checkInside(b)){
             Point2D.Double p = new Point2D.Double();
             //b.setPos(p);
-            b.setMotionY(0);
+            b.setMotionX(0);
             b.setMotionY(0);
             b.setPosX(b.getLastX());
             b.setPosY(b.getLastY());
