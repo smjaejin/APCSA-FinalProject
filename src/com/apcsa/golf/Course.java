4e287889c93f70ddf8e4073e4b7704dee1bf78e5
harrison <harrison@greco.com>
2017-06-06 18:33:30 -0700

    the hole

--- src/com/apcsa/golf/Course.java
+++ src/com/apcsa/golf/Course.java
@@ -13,18 +13,40 @@ public class Course {
     Ball b;
     public ArrayList<Obstacle> courseObjects;
 
-    public Course(){
+    public Course() {
+            courseObjects = new ArrayList();
+            b = new Ball(.75, .6);
+            courseObjects.add(new Mat(0, .6));
+            courseObjects.add(new Hole(.75, -.75));
+            courseObjects.add(new Wall(0, -.75, .6, .08));
+    }
+
+    public Course(int pos) {
+        if (pos == 0) {
         courseObjects = new ArrayList();
-        b = new Ball(.75,.6);
-        courseObjects.add(new Mat(0,.6));
-        courseObjects.add(new Hole(.75,-.75));
-        courseObjects.add(new Wall(0,-.75,.6,.08));
+        b = new Ball(.75, .6);
+        courseObjects.add(new Mat(0, .6));
+        courseObjects.add(new Hole(.75, -.75));
+        courseObjects.add(new Wall(0, -.75, .6, .08));
+        }
+        else if (pos==1){
+            //create course 1
+        }
+        else if (pos==2){
+            //create course 2
+        }
+        else if (pos==3){
+            //create course 3
+        }
+        else if (pos==3){
+            //create course 4
+        }
+        else if (pos==5){
+            //create course 5
+        }
     }
 
-    public Course(int i){
-        courseObjects = new ArrayList<Obstacle>();
 
-    }
 
     public Course(String i){
         courseObjects = new ArrayList<Obstacle>();
--- src/com/apcsa/golf/Courses.java
+++ src/com/apcsa/golf/Courses.java
@@ -13,10 +13,17 @@ public class Courses {
     public Courses(){
         holes = new ArrayList<Course>();
         holeNumber = -1;
+        for (int i:5){Course(i);}
         holes.add(new Course());
-        holes.add(new Course(1));
+       // holes.add(new Course(1));
         holes.add(new Course("i"));
         holes.add(new Course(2.0));
+        holes.add(new Course(('c'));
+        holes.add(new Course(());
+        holes.add(new Course(true));
+        holes.add(new Course((short)1));
+
+
     }
 
     public Course nextCourse(){
